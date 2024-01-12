376db3a2efebc5c5df207ae372187a9aead99a7d
indicodes <80214063+codeswitchstudio@users.noreply.github.com>
2024-01-12 16:41:42 +0000

    added day.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,31 @@
-function getWeather() {
+  // Display the day for current weather
+  function updateDateTime() {
+    var currentTime = dayjs().format('(DD/MM/YYYY)');
+    $('#currentDay').html(currentTime);
+  }
+  updateDateTime();
 
+
+
+function getWeather() {
 //key
 const APIKey = "ae33ce6155933dc0cae19ca26232b426";
 
-//Create a weather dashboard with form inputs.
+// //Create a weather dashboard with form inputs.
 
 
 const locationInput = document.getElementById('location').value;
 
-const todayDiv = document.getElementById('today');
-
-
+// const todayDiv = document.getElementById('today');
 
  // Make sure the user has entered a location
  if (locationInput.trim() === '') {
+
   alert('Please enter a location.');
   return;
 }
 
-//Create a weather dashboard with form inputs.
+// //Create a weather dashboard with form inputs.
 
 
 const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${locationInput}&units=metric&appid=${apiKey}`
@@ -39,34 +46,6 @@ fetch(queryURL)
 
     // Log the resulting object
     console.log(data);
-
-
-  });
-
+  })
 }
-//user input for city name
-// You'll need to adjust your application to accept user input, to store in the city variable that you've created.
 
-//current
-//future
-
-
-  //When a user views the current weather conditions for that city they are presented with:
-  // The city name
-  // The date
-  // An icon representation of weather conditions
-  // The temperature
-  // The humidity
-  // The wind speed
-
-
-
-// When a user views future weather conditions for that city they are presented with a 5-day forecast that displays:
-  // The date
-  // An icon representation of weather conditions
-  // The temperature
-  // The humidity
-
-// When a user click on a city in the search history they are again presented with current and future conditions for that city
-//Local storage
-//class = list-group, id = history
